f9dff9d2baec06033958fb6ad2e1957dac2dcf67
Neil Patel <46456208+neilmp2@users.noreply.github.com>
2022-11-06 18:59:24 -0600

    draft of homepage

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { VStack, HStack, Box, Button, Flex, AppBar } from "@react-native-material/core";
 
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>hello from mt. fuji shizuoka airport</Text>
-      <StatusBar style="auto" />
+      <AppBar title="O'Hare International Airport (ORD)" marginBottom={20} />
+      <VStack alignItems="center" spacing={20}>
+        <HStack spacing={10}>
+          <Button title="Schedule" />
+          <Button title="Airport Information"> </Button>
+        </HStack>
+        <VStack spacing={50}>
+          <Button title="Amenity Finder" />
+          <Button title="Find a Flight" />
+          <Button title="Your Flight Information" />
+
+        </VStack>
+
+
+
+      </VStack>
+
     </View>
+
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
+
+    justifyContent: "flex-start",
+
   },
 });
