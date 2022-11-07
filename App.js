abed553702971ee4c3623709c69eb11bec4b2b21
Renzo <renzol2@illinois.edu>
2022-11-07 13:53:43 -0600

    Remove unused imports

--- App.js
+++ App.js
@@ -1,12 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Provider as PaperProvider } from 'react-native-paper';
 import BottomNavigator from './components/BottomNavigator';
-import HomeScreen from './screens/HomeScreen';
-import PinsScreen from './screens/PinsScreen';
-import SearchScreen from './screens/SearchScreen';
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
