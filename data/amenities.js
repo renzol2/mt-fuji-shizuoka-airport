07a34a0b9dd10cb42b7884d460382371743b0da0
Neil Patel <46456208+neilmp2@users.noreply.github.com>
2022-11-08 22:00:03 -0600

    more data mockup

---
+++ data/amenities.js
@@ -0,0 +1,22 @@
+gates = {
+
+    "Terminal 1": ["B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9"],
+    "Terminal 2": ["C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9"],
+    "Terminal 3": ["G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8", "G9"],
+    "Terminal 5": ["M1", "M2", "M3", "M4", "M5", "M6", "M7", "M8", "M9"]
+}
+
+
+restaurants = {
+    "B": {
+        "B1": { 1: { "name": "Goose Island Beer Company", "hours": "6am-9pm" } },
+        "B2": {},
+        "B3": {},
+        "B4": { 1: { "name": "Goose Island Beer Company", "hours": "6am-9pm" } },
+        "B5": { 1: { "name": "Goose Island Beer Company", "hours": "6am-9pm" } },
+        "B6": { 1: { "name": "Goose Island Beer Company", "hours": "6am-9pm" } },
+        "B7": { 1: { "name": "Goose Island Beer Company", "hours": "6am-9pm" } },
+        "B8": { 1: { "name": "Goose Island Beer Company", "hours": "6am-9pm" } },
+        "B9": { 1: { "name": "Goose Island Beer Company", "hours": "6am-9pm" } },
+    }
+}
