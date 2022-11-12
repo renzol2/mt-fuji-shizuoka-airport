e0ab69ff6854330844f81325e459ffffc53fcfe0
Neil Patel <46456208+neilmp2@users.noreply.github.com>
2022-11-11 20:18:41 -0600

    data mockup

---
+++ data/shops.js
@@ -0,0 +1,13 @@
+/**
+ * @typedef Hours
+ * @property {string} day
+ * @property {string} openingTime
+ * @property {string} closingTime
+ */
+
+/**
+ * @typedef Shop
+ * @property {string} name
+ * @property {Array<Hours>} hours
+ * 
+ */
