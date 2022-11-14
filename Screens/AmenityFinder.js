c87302671b68333b2321456bea65e31c6f5e5f09
Renzo <renzol2@illinois.edu>
2022-11-13 20:17:43 -0600

    Allow user to select different amenities

--- Screens/AmenityFinder.js
+++ Screens/AmenityFinder.js
@@ -7,6 +7,7 @@ import GatePicker from "../Components/GatePicker";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import { useNavigation } from "@react-navigation/native";
+import { AMENITY_TYPES } from "../data/amenityTypes";
 
 /**
  * The Amenity Finder screen allows the user to select a gate
@@ -16,7 +17,16 @@ import { useNavigation } from "@react-navigation/native";
 export default function AmenityFinder() {
     const UNSELECTED_GATE_NAME = "Select a Gate!";
     const [gateName, setGateName] = React.useState(UNSELECTED_GATE_NAME);
+    const [amenityType, setAmenityType] = React.useState(AMENITY_TYPES.ANY);
     const navigation = useNavigation();
+
+    const AMENITY_TYPE_BUTTONS = [
+        { type: AMENITY_TYPES.DINING, buttonTitle: "Dining" },
+        { type: AMENITY_TYPES.LOUNGES, buttonTitle: "Lounges" },
+        { type: AMENITY_TYPES.MEDICAL, buttonTitle: "Medical" },
+        { type: AMENITY_TYPES.RESTROOMS, buttonTitle: "Restrooms" },
+        { type: AMENITY_TYPES.SHOPS, buttonTitle: "Shops" },
+    ];
 
     return (
         <SafeAreaView style={styles.container}>
@@ -25,32 +35,23 @@ export default function AmenityFinder() {
             <GatePicker
                 gateName={gateName}
                 setGateName={setGateName}
+                isUnselected={gateName === UNSELECTED_GATE_NAME}
             />
 
-            <Button
-                style={styles.vStackItem}
-                mode="contained"
-            >
-                Dining
-            </Button>
-            <Button
-                style={styles.vStackItem}
-                mode="contained"
-            >
-                Restrooms
-            </Button>
-            <Button
-                style={styles.vStackItem}
-                mode="contained"
-            >
-                Medical
-            </Button>
-            <Button
-                style={styles.vStackItem}
-                mode="contained"
-            >
-                Lounges
-            </Button>
+            {AMENITY_TYPE_BUTTONS.map(({ type, buttonTitle }) => (
+                <Button
+                    style={
+                        amenityType === type
+                            ? styles.selectedAmenityButton
+                            : styles.amenityTypeButton
+                    }
+                    mode="contained"
+                    onPress={() => setAmenityType(type)}
+                    key={type}
+                >
+                    {buttonTitle}
+                </Button>
+            ))}
 
             <Button
                 mode="contained"
@@ -60,8 +61,11 @@ export default function AmenityFinder() {
                     });
                 }}
                 disabled={gateName === UNSELECTED_GATE_NAME}
+                style={styles.findAmenitiesButton}
             >
-                Find amenities
+                Find{" "}
+                {amenityType === AMENITY_TYPES.ANY ? "amenities" : amenityType}{" "}
+                {gateName === UNSELECTED_GATE_NAME ? "" : `at ${gateName}`}
             </Button>
         </SafeAreaView>
     );
@@ -72,23 +76,18 @@ const styles = StyleSheet.create({
         backgroundColor: colorScheme.backgroundPage,
         flex: 1, // makes component take up all available space (https://reactnative.dev/docs/flexbox)
     },
-    hStack: {
-        flexDirection: "row",
-        marginBottom: 50,
-        justifyContent: "space-evenly",
-        marginTop: 70,
-    },
 
-    vStack: {
-        flexDirection: "column",
-        height: 200,
-        justifyContent: "space-evenly",
-        alignItems: "center",
-        flex: 1,
-    },
-
-    vStackItem: {
+    amenityTypeButton: {
         marginTop: 20,
         backgroundColor: colorScheme.primary,
+    },
+
+    selectedAmenityButton: {
+        marginTop: 20,
+        backgroundColor: colorScheme.light,
+    },
+
+    findAmenitiesButton: {
+        marginTop: 20,
     },
 });
---
+++ data/amenityTypes.js
@@ -0,0 +1,9 @@
+/** Represents all amenity types with information available in the app */
+export const AMENITY_TYPES = {
+    ANY: "any",
+    DINING: "dining",
+    LOUNGES: "lounges",
+    MEDICAL: "medical",
+    RESTROOMS: "restrooms",
+    SHOPS: "shops",
+};
