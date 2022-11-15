f566797c203f0b502f4d17b3062d17c7e14b34ba
Renzo <renzol2@illinois.edu>
2022-11-14 22:24:12 -0600

    Extract restaurant info to card

--- Components/RestaurantsList.js
+++ Components/RestaurantsList.js
@@ -1,6 +1,7 @@
 import { StyleSheet, View } from "react-native";
 import { Colors, IconButton, Surface, Text } from "react-native-paper";
 import { RESTAURANTS } from "../data/restaurants";
+import RestaurantCard from "./RestaurantCard";
 
 /**
  * Displays restaurants at `gate`, or all restaurants if gate is
@@ -9,7 +10,6 @@ import { RESTAURANTS } from "../data/restaurants";
  */
 function RestaurantsList({ gate, pinnedAmenities, setPinnedAmenities }) {
     // Number where 0 = Monday, 1 = Tuesday, ... 6 = Sunday
-    const currentDayIndex = (new Date().getDay() + 6) % 7;
 
     if (RESTAURANTS[gate] === undefined) {
         // Display all restaurants
@@ -25,60 +25,16 @@ function RestaurantsList({ gate, pinnedAmenities, setPinnedAmenities }) {
         return (
             <View>
                 {RESTAURANTS[gate].map(({ name, hours, priceRange, gate }) => {
-                    const currentHours = hours[currentDayIndex];
-                    const { day, openingTime, closingTime } = currentHours;
-                    const isPinned = pinnedAmenities.some((amenity) => name === amenity.name);
                     return (
-                        <Surface
-                            style={styles.restaurantSurface}
+                        <RestaurantCard
                             key={name}
-                        >
-                            <View>
-                                {/* Name of restaurant */}
-                                <Text style={styles.restaurantName}>
-                                    {name}
-                                </Text>
-
-                                {/* Hours */}
-                                <Text style={styles.restaurantHours}>
-                                    {`Hours (${day}): ${openingTime} - ${closingTime}`}
-                                </Text>
-
-                                {/* Price range */}
-                                <Text style={styles.restaurantPriceRange}>
-                                    {`Price range: ${priceRange}`}
-                                </Text>
-                            </View>
-
-                            {/* Pin button */}
-                            <IconButton
-                                style={styles.pinButton}
-                                icon={isPinned ? "pin" : "pin-outline"}
-                                iconColor={Colors.purple100}
-                                size={20}
-                                onPress={() => {
-                                    console.log("Pin this restaurant!");
-                                    if (isPinned) {
-                                        // if the restaurant is pinned, unpin the restaurant
-                                        setPinnedAmenities(pinnedAmenities.filter((amenity) => name !== amenity.name));
-                                    } else {
-                                        // if the restaurant is unpinned, pin the restaurant
-                                        setPinnedAmenities([...pinnedAmenities, { name, hours, priceRange, gate }]);
-                                    }
-                                }}
-                            />
-
-                            {/* Crowdsource button */}
-                            <IconButton
-                                style={styles.pinButton}
-                                icon="car"
-                                iconColor={Colors.purple100}
-                                size={20}
-                                onPress={() =>
-                                    console.log("yay crowdsouricng!")
-                                }
-                            />
-                        </Surface>
+                            name={name}
+                            hours={hours}
+                            priceRange={priceRange}
+                            gate={gate}
+                            pinnedAmenities={pinnedAmenities}
+                            setPinnedAmenities={setPinnedAmenities}
+                        />
                     );
                 })}
             </View>
