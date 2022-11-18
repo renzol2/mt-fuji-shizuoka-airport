9c402c4f3f51580f51bd2e697d1e5209b117cbdb
Shayna Provine <provine2@illinois.edu>
2022-11-17 23:44:07 -0600

    adds dialogs for crowdsource prompt and update

--- Components/RestaurantCard.js
+++ Components/RestaurantCard.js
@@ -27,11 +27,27 @@ export default function RestaurantCard({
     const currentHours = hours[currentDayIndex];
     const { day, openingTime, closingTime } = currentHours;
     const isPinned = pinnedAmenities.some((amenity) => name === amenity.name);
-    const [visible, setVisible] = React.useState(false);
 
-    const showDialog = () => setVisible(true);
-  
-    const hideDialog = () => setVisible(false);
+    const [crowdsourcePrompt, setcrowdsourcePrompt] = React.useState(false);
+    const showCrowdsourcePrompt = () => setcrowdsourcePrompt(true);
+    const hideCrowdsourcePrompt = () => setcrowdsourcePrompt(false);
+
+    const [crowdsourceUpdate, setcrowdsourceUpdate] = React.useState(false);
+    const showCrowdsourceUpdate = () => setcrowdsourceUpdate(true);
+    const hideCrowdsourceUpdate = () => setcrowdsourceUpdate(false);   
+
+    const [timepicker, setTimepicker] = React.useState(false);
+    const showTimepicker = () => setTimepicker(true);
+    const hideTimepicker = () => setTimepicker(false);
+
+    /*const [mydate, setDate] = React.useState(new Date());
+
+    const displaymode = React.useState('time');
+    const changeSelectedDate = (event, selectedDate) => {
+       const currentDate = selectedDate || mydate;
+       setDate(currentDate);
+       setTimepicker(false)
+    }; */
     return (
         <Surface
             style={styles.restaurantSurface}
@@ -89,17 +105,49 @@ export default function RestaurantCard({
                     icon="lightbulb"
                     iconColor={Colors.purple100}
                     size={BUTTON_SIZE}
-                    onPress={showDialog}
+                    onPress={showCrowdsourcePrompt}
                 />
                 <Portal>
-                    <Dialog visible={visible} onDismiss={hideDialog}>
+                    {/* Crowdsource prompt dialog */}
+                    <Dialog visible={crowdsourcePrompt} onDismiss={hideCrowdsourcePrompt}>
                         <Dialog.Title>Hours</Dialog.Title>
                         <Dialog.Content>
-                        <Paragraph>Restaurant</Paragraph>
+                        <Paragraph>Is this info correct?</Paragraph>
+                        <Paragraph>{`Current Hours: ${openingTime} - ${closingTime}`}</Paragraph>
                         </Dialog.Content>
                         <Dialog.Actions>
-                        <Button onPress={hideDialog}>Done</Button>
+                        <Button onPress={hideCrowdsourcePrompt}>Yes</Button>
+                        <Button onPress={showCrowdsourceUpdate}>No</Button>
+                        
                         </Dialog.Actions>
+                    </Dialog>
+                    {/* Crowdsource update dialog */}
+                    <Dialog visible={crowdsourceUpdate} onDismiss={hideCrowdsourceUpdate}>
+                        <Dialog.Title>Hours</Dialog.Title>
+                        <Dialog.Content>
+                        <Paragraph>What are the correct hours?</Paragraph>
+                        <Button onPress={showTimepicker}>Open: {openingTime}</Button>
+                        <Button onPress={showTimepicker}>Close: {closingTime}</Button>
+                        </Dialog.Content>
+                        <Dialog.Actions>
+                        <Button onPress={hideCrowdsourceUpdate}>Confirm</Button>
+                        <Button onPress={hideCrowdsourceUpdate}>Cancel</Button>
+                        
+                        </Dialog.Actions>
+                    </Dialog>
+                    {/* Timepicker dialog */}
+                    <Dialog visible={timepicker} onDismiss={hideTimepicker}>
+                        <Dialog.Title>Hours</Dialog.Title>
+                        <Dialog.Content>
+                        <Paragraph>What are the correct hours?</Paragraph>
+                        {/* <DateTimePicker
+                                value={mydate}
+                                mode={'time'}
+                                is24Hour={false}
+                                display={true}
+                                onChange={changeSelectedDate}
+                            /> */}
+                        </Dialog.Content>
                     </Dialog>
                 </Portal>
             </View>
