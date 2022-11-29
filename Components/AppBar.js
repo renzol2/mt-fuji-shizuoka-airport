efebe765e983731ec33fec535d26fd86e2261a03
Renzo <renzol2@illinois.edu>
2022-11-29 10:38:16 -0600

    Format file

--- Components/AppBar.js
+++ Components/AppBar.js
@@ -1,19 +1,19 @@
-import * as React from 'react';
-import { Appbar } from 'react-native-paper';
-import { colorScheme } from '../Styles';
+import * as React from "react";
+import { Appbar } from "react-native-paper";
+import { colorScheme } from "../Styles";
 import { useNavigation } from "@react-navigation/native";
-
-
-
 
 export default function AppBar() {
     const navigation = useNavigation();
 
     return (
-
-        <Appbar.Header style={{ backgroundColor: colorScheme.primary }} >
-            <Appbar.BackAction onPress={() => { navigation.goBack() }} />
+        <Appbar.Header style={{ backgroundColor: colorScheme.primary }}>
+            <Appbar.BackAction
+                onPress={() => {
+                    navigation.goBack();
+                }}
+            />
             <Appbar.Content title="O'Hare International Airport" />
-        </Appbar.Header>);
-
+        </Appbar.Header>
+    );
 }
